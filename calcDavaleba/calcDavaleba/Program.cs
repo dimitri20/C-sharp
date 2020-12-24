97ab82798394d1a5d3826e08963152272d86c8df
dimitri20 <dimitri.gulua@geolab.edu.ge>
2020-12-24 23:06:41 +0400

    another update

--- calcDavaleba/calcDavaleba/Program.cs
+++ calcDavaleba/calcDavaleba/Program.cs
@@ -10,27 +10,6 @@ namespace calcDavaleba
             //checkEven();
         }
 
-        static void checkEven()
-        {
-            Console.Write("Enter Number: ");
-            string input = Console.ReadLine();
-            try
-            {
-                if (Convert.ToInt32(input) % 2 == 0)
-                {
-                    Console.WriteLine("it's even");
-                }
-                else
-                {
-                    Console.WriteLine("it's odd");
-                }
-            } catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            
-        }
-
         static void calculator()
         {
 
