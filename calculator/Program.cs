a9bd2593ce4dec5c152532654bf0984ab44dad7a
gfkl <gfkl@localhost.localdomain>
2020-12-25 22:25:40 +0400

    updated homework

---
+++ calculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter First Number: ");
+            double first = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operation:  ");
+            string operation = Console.ReadLine();
+
+            Console.Write("Enter Second Number: ");
+            double second = Convert.ToDouble(Console.ReadLine());
+
+            double answer = 0;
+            bool isZero = false;
+
+            if(operation == "+"){
+                answer = first + second;
+            } else if(operation == "-"){
+                answer = first - second;
+            } else if (operation == "*"){
+                answer = first * second;
+            } else if(operation == "/"){
+                if(second != 0){
+                    answer = first/second;
+                    isZero = false;
+                } else {
+                    isZero = true;
+                }
+            }
+
+            if(isZero){
+                Console.WriteLine("You cannot divide by zero");
+            } else {
+                Console.WriteLine("{0} {1} {2} = {3} ", first, operation, second, answer);
+            }
+
+        }
+    }
+}
---
+++ checkEven/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace checkEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number: ");
+            string input = Console.ReadLine();
+            int convertedInput = Convert.ToInt32(input);
+            if(convertedInput % 2 == 0){
+                Console.WriteLine("even");
+            } else if(convertedInput % 2 == 1){
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
